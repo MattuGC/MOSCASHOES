016da4fff0092eda86a302344792e05c7475af5d
MattuGC <pic.k.life.contacto@gmail.com>
2024-08-22 20:23:06 -0300

    Welcome pop-up and home

--- static/style/js/general.js
+++ static/style/js/general.js
@@ -1,12 +1,15 @@
 // general.js
 
-document.querySelectorAll('nav a').forEach(link => {
+document.querySelectorAll('nav #left-menu a').forEach(link => {
     link.addEventListener('click', (event) => {
       event.preventDefault();
       const targetId = event.target.getAttribute('href');
       document.querySelector(targetId).scrollIntoView({
         behavior: 'smooth'
       });
+      document.getElementById("home").style.opacity = "0";
+      document.documentElement.style.setProperty("--section-opacity", "0");
+      document.getElementById(targetId.substring(1)).style.opacity = "1";
     });
   });
 
@@ -15,4 +18,16 @@ document.querySelectorAll('nav a').forEach(link => {
 document.addEventListener("DOMContentLoaded", () => {
     loadProducts();
     setupLanguageSelector();
+});
+
+const home_button = document.getElementById("home-button");
+
+home_button.addEventListener("click", (event) => {
+  document.getElementById("home").style.opacity = "1";
+});
+
+const contact_button = document.getElementById("contact-button");
+
+contact_button.addEventListener("click", (event) => {
+  document.getElementById("contact").style.opacity = "1";
 });
