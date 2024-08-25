5a8c85a7d72b219b5940ff69a1fa0b306fea2ee9
MattuGC <pic.k.life.contacto@gmail.com>
2024-08-25 01:24:58 -0300

    Fixed pop-up, header, and home
    Pending right menu

--- static/style/js/general.js
+++ static/style/js/general.js
@@ -1,33 +1,16 @@
 // general.js
 
-document.querySelectorAll('nav #left-menu a').forEach(link => {
-    link.addEventListener('click', (event) => {
-      event.preventDefault();
-      const targetId = event.target.getAttribute('href');
-      document.querySelector(targetId).scrollIntoView({
-        behavior: 'smooth'
-      });
-      document.getElementById("home").style.opacity = "0";
-      document.documentElement.style.setProperty("--section-opacity", "0");
-      document.getElementById(targetId.substring(1)).style.opacity = "1";
-    });
-  });
-
-// general.js
-
 document.addEventListener("DOMContentLoaded", () => {
     loadProducts();
     setupLanguageSelector();
 });
 
-const home_button = document.getElementById("home-button");
-
-home_button.addEventListener("click", (event) => {
-  document.getElementById("home").style.opacity = "1";
-});
-
-const contact_button = document.getElementById("contact-button");
-
-contact_button.addEventListener("click", (event) => {
-  document.getElementById("contact").style.opacity = "1";
-});
+function goTo(page) {
+  console.log("Going to " + page + "!");
+  if(document.getElementsByClassName("active").length > 0) {
+    document.getElementsByClassName("active")[0].classList.remove("active");
+  }
+  if(page!=="home") {
+    document.getElementById(page).classList.add("active");
+  }
+}
